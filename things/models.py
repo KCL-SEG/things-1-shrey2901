5680fccf0853af787bf9d161442f2e4a07480189
Shrey <shreyansh290103@outlook.com>
2023-10-26 00:43:47 +0100

    Exercise 2: Make changes to the model of the app

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models import Model
 
-class Thing(Model):
+class Thing():
     name = models.CharField()
     description = models.TextField()
     quantity = models.IntegerField()
