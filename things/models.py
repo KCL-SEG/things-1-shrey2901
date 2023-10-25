31a3b421735905d3f6d77ddfdd18974e2cfe398f
Shrey <shreyansh290103@outlook.com>
2023-10-26 00:52:44 +0100

    Exercise 2: Make changes to the model of the app

--- things/models.py
+++ things/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models import Model
 
 class Thing():
-    name = models.CharField()
-    description = models.CharField()
-    quantity = models.IntegerField()
+    def __init__(self):
+        name = models.CharField(max_length=30,unique = True, blank = False )
+        description = models.CharField(max_length=120,blank =True)
+        quantity = models.IntegerField(min_value=0, max_value=100)
