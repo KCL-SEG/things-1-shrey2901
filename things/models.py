18a4e8a0a23bad73e53c5e594901b93b57326c0b
Shrey <shreyansh290103@outlook.com>
2023-10-26 13:55:57 +0100

    Use CharField instead of TextField

--- things/models.py
+++ things/models.py
@@ -5,4 +5,4 @@ class Thing(models.Model):
     name = models.CharField(unique =True, max_length= 30,blank=False)
     description = models.CharField(max_length= 120,blank=True)
     quantity = models.IntegerField(validators=
-    [MinValueValidator(1), MaxValueValidator(100)])
+    [MinValueValidator(0), MaxValueValidator(100)])
